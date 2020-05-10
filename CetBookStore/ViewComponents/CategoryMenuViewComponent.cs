6d65357a2dd09b1ef353f961a9ada71936559330
KadirErhanAkcay <kadir_erhan@hotmail.com>
2020-05-11 01:15:02 +0300

    Add project files.

---
+++ CetBookStore/ViewComponents/CategoryMenuViewComponent.cs
@@ -0,0 +1,35 @@
+﻿using CetBookStore.Data;
+using CetBookStore.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CetBookStore.ViewComponents
+{
+    public partial class CategoryMenuViewComponent : ViewComponent
+    {
+        private readonly ApplicationDbContext context;
+
+        public CategoryMenuViewComponent(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(bool forSearch = false)
+        {
+            var cats = await context.Categories.ToListAsync();
+            if (forSearch)
+            {
+                return View("categorysearch", cats);
+            }
+            else
+            {
+                return View(cats);
+            }
+
+        }
+    }
+}
---
+++ CetBookStore/ViewModel/SearchViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CetBookStore.ViewModel
+{
+    public class SearchViewModel
+    {
+        [Display(Name = "Arama Metni")]
+        public string SearchText { get; set; }
+        [Display(Name = "Açıklamalarda Ara")]
+        public bool SearchInDescription { get; set; }
+        [Display(Name = "Kategori Seçimi")]
+        public int? CategoryId { get; set; }
+
+
+        [Display(Name = "Minimum Fiyat Giriniz")]
+        public decimal? MinPrice { get; set; }
+        [Display(Name = "Maksimum Fiyat Giriniz")]
+        public decimal? MaxPrice { get; set; }
+        public List<Models.Book> Results { get; set; }
+    }
+}
